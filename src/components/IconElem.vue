38d45163ccacbde89fd97df418962d3f392ff30d
Ernandes R de Souza <70029077+ernandesrs@users.noreply.github.com>
2023-02-09 13:01:24 -0400

    Component: icon

---
+++ src/components/IconElem.vue
@@ -0,0 +1,26 @@
+<template>
+    <span class="icon" :class="[iconClass]"></span>
+</template>
+
+<script>
+export default {
+    props: {
+        icon: {
+            type: String,
+            default: 'bi bi-app'
+        },
+    },
+
+    computed: {
+        iconClass() {
+            return this.icon;
+        }
+    },
+}
+</script>
+
+<style lang="css" scoped>
+.icon {
+    @apply inline-block pointer-events-none;
+}
+</style>
