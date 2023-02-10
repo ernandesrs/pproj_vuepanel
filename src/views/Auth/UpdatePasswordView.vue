8fe3169aba3a7f2989c92929881d649fcc395c86
Ernandes R de Souza <70029077+ernandesrs@users.noreply.github.com>
2023-02-10 13:30:17 -0400

    Update password view

--- src/views/Auth/UpdatePasswordView.vue
+++ src/views/Auth/UpdatePasswordView.vue
@@ -1,21 +1,50 @@
 <template>
-    <div>
-        AUTH UPDATE PASSWORD
+
+    <div class="mb-3">
+        <InputGroupForm :attrs="{
+            id: 'password',
+            name: 'password',
+            type: 'password'
+        }" label="Senha:" v-model="form.password" :error-message="form.errors.password" />
     </div>
+
+    <div class="mb-3">
+        <InputGroupForm :attrs="{
+            id: 'password_confirmation',
+            name: 'password_confirmation',
+            type: 'password'
+        }" label="Confirmar senha:" v-model="form.password_confirmation" :error-message="form.errors.password_confirmation" />
+    </div>
+
+    <div class="text-center">
+        <DefaultButton text="Atualizar senha"
+            icon="bi bi-check-lg" :loading="form.submitting" />
+    </div>
+
 </template>
 
 <script>
+
+import InputGroupForm from '../../components/Form/InputGroupForm.vue';
+import DefaultButton from '../../components/Button/DefaultButton.vue';
+
 export default {
-    setup() {
-
-
-        return {}
+    components: { InputGroupForm, DefaultButton },
+    data() {
+        return {
+            form: {
+                password: null,
+                password_confirmation: null,
+                errors: {
+                },
+                submitting: false
+            }
+        };
     },
-
     created() {
-        this.$store.commit("pageTitle", "Atualizar senha");
         this.$store.commit("pageIcon", "bi bi-check-circle");
-    }
+        this.$store.commit("pageTitle", "Atualizar senha");
+    },
 }
 </script>
 
