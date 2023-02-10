f454e4b3b845e8e2fd1819fba2ecc7114290fb25
Ernandes R de Souza <70029077+ernandesrs@users.noreply.github.com>
2023-02-10 11:20:45 -0400

    Component: button variants

--- src/components/Button/DefaultButton.vue
+++ src/components/Button/DefaultButton.vue
@@ -41,10 +41,34 @@ export default {
 
 <style lang="css" scoped>
 .button {
-    @apply px-5 py-2 rounded disabled:bg-opacity-50 hover:bg-opacity-90 duration-150 hover:ease-in;
+    @apply px-5 py-2 rounded disabled:bg-opacity-50 hover:bg-opacity-80 duration-150 hover:ease-in shadow-md;
 }
 
 .button-primary {
     @apply bg-primary text-light-light;
+}
+
+.button-secondary {
+    @apply bg-secondary text-light-light;
+}
+
+.button-light {
+    @apply bg-light text-dark-light;
+}
+
+.button-dark {
+    @apply bg-dark text-light;
+}
+
+.button-success {
+    @apply bg-emerald-500 text-light;
+}
+
+.button-danger {
+    @apply bg-red-400 text-light;
+}
+
+.button-warning {
+    @apply bg-orange-400 text-light;
 }
 </style>
