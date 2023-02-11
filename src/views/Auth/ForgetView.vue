ce9def42b216b7f19a0b4ac80bf0c333932fd689
Ernandes R de Souza <70029077+ernandesrs@users.noreply.github.com>
2023-02-11 17:35:01 -0400

    Forget password

--- src/views/Auth/ForgetView.vue
+++ src/views/Auth/ForgetView.vue
@@ -1,25 +1,30 @@
 <template>
-    <div class="mb-3">
-        <InputGroupForm :attrs="{
-            id: 'email',
-            name: 'email',
-            type: 'email'
-        }" label="E-mail" v-model="form.email" :error-message="form.errors.email" />
-    </div>
+    <FormElem action="/auth/forget-password" method="post" :data="form" :callbacks="{
+        success: success
+    }">
+        <div class="mb-3">
+            <InputGroupForm :attrs="{
+                id: 'email',
+                name: 'email',
+                type: 'email'
+            }" label="E-mail" v-model="form.email" :error-message="form.errors.email" />
+        </div>
 
-    <div class="text-center">
-        <DefaultButton text="Recuperar senha"
-            icon="bi bi-check-lg" :loading="form.submitting" />
-    </div>
+        <div class="text-center">
+            <DefaultButton text="Recuperar senha" icon="bi bi-check-lg"
+                :loading="form.submitting" />
+        </div>
+    </FormElem>
 </template>
 
 <script>
 
 import DefaultButton from '../../components/Button/DefaultButton.vue';
+import FormElem from '../../components/Form/FormElem.vue';
 import InputGroupForm from '../../components/Form/InputGroupForm.vue';
 
 export default {
-    components: { InputGroupForm, DefaultButton },
+    components: { InputGroupForm, DefaultButton, FormElem },
     data() {
         return {
             form: {
@@ -34,7 +39,12 @@ export default {
     created() {
         this.$store.commit("pageTitle", "Esquecia a senha");
         this.$store.commit("pageIcon", "bi bi-shield-lock");
-    }
+    },
+    methods: {
+        success() {
+            // 
+        }
+    },
 }
 </script>
 
