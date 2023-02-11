fd38cb18c9fcbf483184233a9ef9267c77f88777
Ernandes R de Souza <70029077+ernandesrs@users.noreply.github.com>
2023-02-11 17:48:05 -0400

    Update password

--- src/views/Auth/UpdatePasswordView.vue
+++ src/views/Auth/UpdatePasswordView.vue
@@ -1,25 +1,31 @@
 <template>
 
-    <div class="mb-3">
-        <InputGroupForm :attrs="{
-            id: 'password',
-            name: 'password',
-            type: 'password'
-        }" label="Senha:" v-model="form.password" :error-message="form.errors.password" />
-    </div>
+    <FormElem action="/auth/update-password" method="put" :data="form" :callbacks="{
+        success: success
+    }">
+        <div class="mb-3">
+            <InputGroupForm :attrs="{
+                id: 'password',
+                name: 'password',
+                type: 'password'
+            }" label="Senha:" v-model="form.password"
+                :error-message="form.errors.password" />
+        </div>
 
-    <div class="mb-3">
-        <InputGroupForm :attrs="{
-            id: 'password_confirmation',
-            name: 'password_confirmation',
-            type: 'password'
-        }" label="Confirmar senha:" v-model="form.password_confirmation" :error-message="form.errors.password_confirmation" />
-    </div>
+        <div class="mb-3">
+            <InputGroupForm :attrs="{
+                id: 'password_confirmation',
+                name: 'password_confirmation',
+                type: 'password'
+            }" label="Confirmar senha:" v-model="form.password_confirmation"
+                :error-message="form.errors.password_confirmation" />
+        </div>
 
-    <div class="text-center">
-        <DefaultButton text="Atualizar senha"
-            icon="bi bi-check-lg" :loading="form.submitting" />
-    </div>
+        <div class="text-center">
+            <DefaultButton text="Atualizar senha" icon="bi bi-check-lg"
+                :loading="form.submitting" />
+        </div>
+    </FormElem>
 
 </template>
 
@@ -27,12 +33,14 @@
 
 import InputGroupForm from '../../components/Form/InputGroupForm.vue';
 import DefaultButton from '../../components/Button/DefaultButton.vue';
+import FormElem from '../../components/Form/FormElem.vue';
 
 export default {
-    components: { InputGroupForm, DefaultButton },
+    components: { InputGroupForm, DefaultButton, FormElem },
     data() {
         return {
             form: {
+                token: this.$route?.query?.token,
                 password: null,
                 password_confirmation: null,
                 errors: {
@@ -41,9 +49,18 @@ export default {
             }
         };
     },
+    beforeCreate() {
+        if (!this.$route?.query?.token)
+            this.$router.push({ name: "auth.login" })
+    },
     created() {
         this.$store.commit("pageIcon", "bi bi-check-circle");
         this.$store.commit("pageTitle", "Atualizar senha");
+    },
+    methods: {
+        success() {
+            this.$router.push({ name: "auth.login" });
+        }
     },
 }
 </script>
