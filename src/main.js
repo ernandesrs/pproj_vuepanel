f6654cb63011d439f065cb9bc89780e4d7741717
Ernandes R de Souza <70029077+ernandesrs@users.noreply.github.com>
2023-02-11 11:37:25 -0400

    Axios installation and configuration

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import store from './store'
+import axios from './plugins/axios.js'
 import App from './App.vue'
 import router from './router'
 
@@ -7,6 +8,9 @@ import './assets/main.css'
 
 const app = createApp(App)
 
-app.use(router).use(store);
+app.config.globalProperties.$store = store
+app.config.globalProperties.$axios = axios
+
+app.use(router)
 
 app.mount('#app')
