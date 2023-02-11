f7c3ae94de74402d74ef21aa9da4c66b8b7dadcf
Ernandes R de Souza <70029077+ernandesrs@users.noreply.github.com>
2023-02-11 12:22:00 -0400

    Axios config

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -1,7 +1,13 @@
 import axios from 'axios';
 
+axios.defaults.baseURL = 'http://127.0.0.1:8000/api/v1';
+
 export default {
-    get: () => {
-        return axios;
+    request: (action, data, method = 'get') => {
+        return axios.request({
+            method: method,
+            url: action,
+            data: data
+        });
     }
 };
