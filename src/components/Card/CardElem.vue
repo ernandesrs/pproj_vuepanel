eb9255a3b269394df3a45a0619ebcb6e86d9e8f5
Ernandes R de Souza <70029077+ernandesrs@users.noreply.github.com>
2023-02-16 07:32:03 -0400

    Component: card elem

---
+++ src/components/Card/CardElem.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="border border-dark border-opacity-25 rounded-lg px-4 py-2">
+        <h3 v-if="title && !$slots.header" class="text-lg font-semibold"
+            :class="titleClass" v-html="title">
+        </h3>
+        <div v-else class="pt-2">
+            <slot name="header" />
+        </div>
+        <div class="py-3">
+            <slot name="content" />
+        </div>
+        <div class="border-t border-dark border-opacity-25"></div>
+        <div class="py-2">
+            <slot name="footer" />
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+    props: {
+        title: {
+            type: String,
+            default: null
+        },
+        titleClass: {
+            type: String,
+            default: null
+        }
+    },
+}
+
+</script>
--- src/views/App/HomeView.vue
+++ src/views/App/HomeView.vue
@@ -1,19 +1,65 @@
 <template>
-    <div>
-        <p>APP HOME</p>
+    <div v-if="loadingContent" class="text-center animate-pulse">
+        <IconElem icon="bi bi-arrow-clockwise animate-spin text-4xl" />
+        <p class="text-lg">Aguarde...</p>
+    </div>
+    <div v-else class="flex justify-center gap-4">
+
+        <!-- subscriptions -->
+        <div class="basis-1/2 lg:basis-1/3">
+            <CardElem :title="`${{
+                undefined: 'Sem assinatura',
+                canceled: 'Assinatura cancelada',
+                pending: 'Aguardando pagamento',
+                active: 'Assinatura ativa',
+                ended: 'Assinatura vencida',
+            }[activeSubscription?.status]}`" :titleClass="{
+    'text-red-500': !activeSubscription || activeSubscription?.status == 'canceled',
+    'text-sky-300': activeSubscription?.status == 'pending',
+    'text-emerald-500': activeSubscription?.status == 'active',
+    'text-yellow-300': activeSubscription?.status == 'ended',
+}">
+                <template v-slot:content>
+                    <p class="text-sm" v-if="activeSubscription?.status == 'active'">Você
+                        possui uma assinatura válida até {{ activeSubscription.ends_in }}
+                    </p>
+                    <p class="text-sm" v-else>...</p>
+                </template>
+                <template v-slot:footer>
+                    <LinkElem icon="bi bi-clock" text="Histórico de assinaturas"
+                        :to="{ name: 'app.subscriptions' }" />
+                </template>
+            </CardElem>
+        </div>
+        <!-- /subscriptions -->
+        <!-- /cards -->
+
     </div>
 </template>
 
 <script>
+
+import CardElem from '../../components/Card/CardElem.vue';
+import IconElem from '../../components/IconElem.vue';
+import LinkElem from '../../components/LinkElem.vue';
+
 export default {
-    setup() {
-
-
-        return {}
+    components: { IconElem, LinkElem, CardElem },
+    data() {
+        return {
+            loadingContent: true,
+            activeSubscription: {
+                status: 'active',
+                ends_in: '09/12/2023'
+            }
+        };
+    },
+    mounted() {
+        this.loadingContent = false;
     }
 }
 </script>
 
-<style lang="scss" scoped>
+<style lang="css" scoped>
 
 </style>
